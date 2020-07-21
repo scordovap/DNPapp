23eae48e3936812ce673ee94710f6f68b7878d81
Steven Córdova <67441250+vanz191@users.noreply.github.com>
2020-07-21 09:26:21 -0500

    Version_3_arreglando_metodos_fase_III

---
+++ app/src/main/java/com/ps/dnpapp/Model/Sensores.java
@@ -0,0 +1,17 @@
+package com.ps.dnpapp.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ps.dnpapp.R;
+
+public class Sensores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensores);
+
+    }
+}
